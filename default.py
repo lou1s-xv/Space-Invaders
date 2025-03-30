6cf3e40a1c682a89405ae88532ad2e8fa0174f97
KingBash714 <sauls.bashir2@gmail.com>
2025-03-30 08:36:36 +0200

    Setup

---
+++ default.py
@@ -0,0 +1,12 @@
+import sys
+import math
+import stdio, stdarray, stdrandom, stddraw, stdaudio  # type: ignore
+
+
+def main() -> None:  # Need the return type for mypy to type-check the body
+
+    stdio.writeln("hello world")
+
+
+if __name__ == "__main__":
+    main()
