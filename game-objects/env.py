b6ddfd5fce3540a8a827e77578b1614aec0bc76b
lou1s-xv <107552608+lou1s-xv@users.noreply.github.com>
2025-04-08 14:18:44 +0200

    Create env.py

---
+++ game-objects/env.py
@@ -0,0 +1,17 @@
+import stddraw
+import shooter, gamewindow
+
+BUNKER_NR = 3
+BUNKER_THICKNESS = 0.1
+BUNKER_DIST = 0.15
+BUNKER_WIDTH = (X_MAX - X_MIN) / BUNKER_NR
+
+def draw_bunkers(): # if shooter behind bunker, disable shooting
+    for i in range(BUNKER_NR):
+        stddraw.filledRectangle(X_MIN + BUNKER_WIDTH * (2 * i + 1), Y_SHOOTER + SHOOTER_SIZE + BUNKER_DIST, X_MIN + BUNKER_WIDTH, BUNKER_THICKNESS)
+    
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
