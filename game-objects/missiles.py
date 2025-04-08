646ca03bee92d53bc3bd455448cbd0b96cae74ad
lou1s-xv <107552608+lou1s-xv@users.noreply.github.com>
2025-04-08 14:30:01 +0200

    Update missiles.py
    Added bunker collisions

--- game-objects/missiles.py
+++ game-objects/missiles.py
@@ -1,7 +1,8 @@
 import sys
 import math
 import stdio, stdrandom, stddraw, stdaudio, stdarray
-import gamewindow, enemies
+import gamewindow, env, shooter
+import enemies as en
 
 # X_MIN, X_MAX, Y_MIN, Y_MAX, FPS -> gamewindow.py
 
@@ -25,8 +26,8 @@ class Missile:
         self.ang = ang
 
     def update_pos(self, FPS): # FPS is a constant stored somewhere idk (maybe in some module to manage the game window)
-        self.x += MISSILE_VELOCITY * math.sin(self.ang) / FPS
-        self.y += MISSILE_VELOCITY * math.cos(self.ang) / FPS
+        self.x += MISSILE_VELOCITY * math.sin(self.ang) / gamewindow.FPS
+        self.y += MISSILE_VELOCITY * math.cos(self.ang) / gamewindow.FPS
 
     def draw_missile(self):
         stddraw.setPenColor(MISSILE_COLOR)
@@ -36,12 +37,17 @@ class Missile:
 def detect_collision(missiles, enemies): # destroys missiles and enemies in collisions, as well as out of bounds missiles
     for i in range(len(missiles)):
         if missiles[i].x < X_MIN or missiles[i].x > X_MAX or missiles[i].y < Y_MIN or missiles[i].y > Y_MAX:
-            missiles.pop(i)
+            missiles.pop(i) 
         else:
+            for k in range(env.BUNKER_NR):
+                if X_MIN + (BUNKERWIDTH * (2 * k + 1)) < missiles[i].x < X_MIN + (BUNKERWIDTH * (2 * (k + 1))) and Y_MIN + BUNKER_DIST < missiles[i].y < Y_MIN + BUNKER_DIST + BUNKER_THICKNESS:
+                    missiles.pop[i]
+                    break
             for k in range(len(enemies)):
-                if (enemies[k].x - (ENEMY_SIZE / 2) < missiles[i].x < enemies[k].x + (ENEMY_SIZE / 2)) and (enemies[k].y - (ENEMY_SIZE / 2) < missiles[i].y < enemies[k].y + (ENEMY_SIZE / 2)):
+                if (enemies[k].x - (en.ENEMY_SIZE / 2) < missiles[i].x < enemies[k].x + (en.ENEMY_SIZE / 2)) and (enemies[k].y - (en.ENEMY_SIZE / 2) < missiles[i].y < enemies[k].y + (en.ENEMY_SIZE / 2)):
                     enemies.pop(k)
                     missiles.pop(i)
+                    break
     
 # TODO: make module tests
 
