4bec9886c20cbbb48cda27f4aab43a63fdf74910
lou1s-xv <107552608+lou1s-xv@users.noreply.github.com>
2025-04-08 14:44:06 +0200

    Update shooter.py

--- game-objects/shooter.py
+++ game-objects/shooter.py
@@ -2,6 +2,8 @@ import sys
 import math
 import stdio, stdarray, stdrandom, stddraw, stdaudio  # type: ignore
 
+SHOOTER_SIZE = 0.1
+
 
 def main() -> None:  # Need the return type for mypy to type-check the body
 
