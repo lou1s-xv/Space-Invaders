0b29ef3880693ca7ce03bd706a3b85967793531a
lou1s-xv <107552608+lou1s-xv@users.noreply.github.com>
2025-04-12 16:15:35 +0200

    Update levellayout.py

--- game-objects/levellayout.py
+++ game-objects/levellayout.py
@@ -1,12 +1,28 @@
-#Data for reference:
+import gamewindow
 
-rows_1 = 2
-cols_1 = 8
-rows_2 = 2
-cols_2 = 8
-rows_3 = 2
-cols_3 = 8
-d = 0.35
-vx = 0.025
-vy = -0.10
-pvx = -0.0125
+# LVL 1
+
+#rows_1 = 2
+#cols_1 = 8
+#rows_2 = 2
+#cols_2 = 8
+#rows_3 = 2
+#cols_3 = 8
+#d = 0.35
+#vx = 0.025
+#vy = -0.10
+#pvx = -0.0125
+
+# LVL 2
+
+# LVL 3
+
+# LVL 4
+
+# LVL 5 - BOSS
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
