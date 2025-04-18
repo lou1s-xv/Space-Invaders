396f5a86e30d4d2b0f0637d8e1ddb8145b0ebd36
Orey07 <kingsleymabunda0707@gmail.com>
2025-04-18 23:39:45 +0200

    Update shooter.py

--- game-objects/shooter.py
+++ game-objects/shooter.py
@@ -8,31 +8,46 @@ import gamewindow as gw
 # constants
 SHOOTER_SIZE = 0.1
 SHOOTER_DIST = 0.1
+SHOOTER_SPEED = 3
 SHOOTER_COLOR = stddraw.BLUE
 
 class SHOOTER:
     x : float
     y : float
     
-    def shooter_motion(self, pos_x,pos_y):
+    def shooter_motion(self, pos_x, pos_y):
+        self.x = pos_x
+        self.y = pos_y
+
+        # Boundary conditions
+        pos_x = max(SHOOTER_SIZE/2,min(gw.X_MAX - SHOOTER_SIZE/2, pos_x))
+        pos_y = max(SHOOTER_SIZE/2,min(gw.Y_MAX - SHOOTER_SIZE/2, pos_y))
+    
+    def move_shooter(self, pos_x, pos_y):
         self.x = pos_x
         self.y = pos_y
         
-        self.x = gw.X_MAX//2 - SHOOTER_SIZE//2
-        self.y = gw.Y_MAX - SHOOTER_SIZE - 1
-    
-    def mov_shooter(self,FPS):
+        keys = stddraw.getKeysPressed()
+        if keys[stddraw.K_LEFT]:
+            pos_x -= SHOOTER_SPEED
+        if keys[stddraw.K_RIGHT]:
+            pos_x += SHOOTER_SPEED
+        if keys[stddraw.K_DOWN]:
+            pos_y -= SHOOTER_SPEED
+        if keys[stddraw.K_UP]:
+            pos_y += SHOOTER_SPEED
+        if keys[stddraw.K_q]:
+            break
+        
+    def draw_shooter(self):
+        stddraw.setPenColor(SHOOTER_COLOR)
+        stddraw.filledSquare(self.x,self.y, SHOOTER_SIZE/2)
+        
+
         
 
 def main() -> None:  # Need the return type for mypy to type-check the body
 
-   
-    
-    
-    
-    
-
-    stdio.writeln("hello world")
 
 
 if __name__ == "__main__":
