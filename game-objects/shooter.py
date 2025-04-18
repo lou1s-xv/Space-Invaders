4ccb24a8c976259564a16a7b7f7d9a884274d8a3
Orey07 <kingsleymabunda0707@gmail.com>
2025-04-18 17:49:14 +0200

    Update shooter.py

--- game-objects/shooter.py
+++ game-objects/shooter.py
@@ -1,11 +1,36 @@
 import sys
 import math
 import stdio, stdarray, stdrandom, stddraw, stdaudio  # type: ignore
+import gamewindow as gw
 
+# X_MIN, X_MAX, Y_MIN, Y_MAX, FPS -> gamewindow.py
+
+# constants
 SHOOTER_SIZE = 0.1
 SHOOTER_DIST = 0.1
+SHOOTER_COLOR = stddraw.BLUE
+
+class SHOOTER:
+    x : float
+    y : float
+    
+    def shooter_motion(self, pos_x,pos_y):
+        self.x = pos_x
+        self.y = pos_y
+        
+        self.x = gw.X_MAX//2 - SHOOTER_SIZE//2
+        self.y = gw.Y_MAX - SHOOTER_SIZE - 1
+    
+    def mov_shooter(self,FPS):
+        
 
 def main() -> None:  # Need the return type for mypy to type-check the body
+
+   
+    
+    
+    
+    
 
     stdio.writeln("hello world")
 
