7eb5839863f711f9b75f6abed6b2e55b87201a2b
Orey07 <kingsleymabunda0707@gmail.com>
2025-04-21 16:12:03 +0200

    Update shooter.py

--- game-objects/shooter.py
+++ game-objects/shooter.py
@@ -5,53 +5,89 @@ import gamewindow as gw
 
 # X_MIN, X_MAX, Y_MIN, Y_MAX, FPS -> gamewindow.py
 
-# constants
-SHOOTER_SIZE = 0.1
-SHOOTER_DIST = 0.1
-SHOOTER_SPEED = 3
-SHOOTER_COLOR = stddraw.BLUE
-
-# initial position
-pos_x = gw.X_MAX/2
-pos_y = 0.1
-
-class SHOOTER:
+class Shooter:
     x : float
     y : float
     
-    def shooter_motion(self, pos_x, pos_y):
-        self.x = pos_x
-        self.y = pos_y
+    def initial(shooter, pos_x, pos_y):
+        shooter.x = pos_x
+        shooter.y = pos_y
+
+        # initial position
+        pos_x = gw.X_MAX/2
+        pos_y = 0.1
+
+        shooter.size = 0.5
+        shooter.speed = 0.2
+        shooter.color = stddraw.RED
+        shooter.health = 100
+        
+    def draw(shooter):
         
         image_path = Picture('shooter.JPG')
+        stddraw.picture(image_path,pos_x,pos_y) 
 
-        # main loop
-        while True:
-            stddraw.clear()
-            stddraw.picture(image_path,pos_x,pos_y) 
+        # Draw Health bar
+
+        bar_x = 0.1
+        bar_y = 0.02
+        width = shooter.x - bar_x/2
+        height = shooter.y + shooter.size +0.05
+
+        # Health Bar background
+        stddraw.setPenColor.(stddarray.BLUE)
+        stddraw.filledRectangle(width,height,bar_x,bar_y)
+
+        # Health color
+        health_width = bar_x * (shooter.health/50)
+        health_color = stddraw.GREEN
+
+        if shooter.health < 50:
+            health_color = YELLOw
+        elif shooter.health < 20:
+            health_color = stddraw.RED
             
-            if stddraw.hasNextKeyTyped():
-                key = stddraw.nextKeyTyped()
-                if key == stddraw.KEYUP:
-                    pos_y += SHOOTER_SPEED
-                elif key == stddraw.KEYDOWN:
-                    pos_y -= SHOOTER_SPEED
-                elif key == stddraw.K_LEFT:
-                    pos_x -= SHOOTER_SPEED
-                elif key == stddraw.K_RIGHT:
-                    pos_x += SHOOTER_SPEED
-                elif key == stddraw.K_ESCAPE:
-                    break
+        stddraw.setPenColor(Health_Color)
+        stddraw.filledRectangle(width,height,health_width,bar_y)
+
+        # Health bar border
+        stddraw.setPenColor(stddraw.BLACK)
+        stddraw.rectangle(width,height,bar_x,bar_y)
+    
+    def damage(shooter, damage):
+        shooter.health =max(0, shooter.health-damage)
+        if shooter.health == 0:
+            stdio.writeln("Shooter is dead!")
+
+            
+def main():
+
+    stddraw.clear()
+    
+    # main loop
+    while True:
+        if stddraw.hasNextKeyTyped():
+            key = stddraw.nextKeyTyped()
+            if key == stddraw.KEYUP:
+                pos_y += SHOOTER_SPEED
+            elif key == stddraw.KEYDOWN:
+                pos_y -= SHOOTER_SPEED
+            elif key == stddraw.K_LEFT:
+                pos_x -= SHOOTER_SPEED
+            elif key == stddraw.K_RIGHT:
+                pos_x += SHOOTER_SPEED
+            elif key == 'd':
+                shooter.damage(10)
+            elif key == stddraw.K_ESCAPE:
+                break
                     
-            # Boundary conditions
-            pos_x = max(SHOOTER_SIZE/2,min(gw.X_MAX - SHOOTER_SIZE/2, pos_x))
-            pos_y = max(SHOOTER_SIZE/2,min(gw.Y_MAX - SHOOTER_SIZE/2, pos_y))
-            
-            stddraw.show()
-    
-    
-def main() -> None:  # Need the return type for mypy to type-check the body
+        # Boundary conditions
+        pos_x = max(SHOOTER_SIZE/2,min(gw.X_MAX - SHOOTER_SIZE/2, pos_x))
+        pos_y = max(SHOOTER_SIZE/2,min(gw.Y_MAX - SHOOTER_SIZE/2, pos_y))
 
+        shooter.draw()
+            
+        stddraw.show(20)
 
 
 if __name__ == "__main__":
