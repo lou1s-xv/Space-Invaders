801092b4a778ab3c05deedb2e50db35b76aa28e9
Orey07 <kingsleymabunda0707@gmail.com>
2025-04-22 17:43:33 +0200

    Update shooter.py

--- game-objects/shooter.py
+++ game-objects/shooter.py
@@ -3,92 +3,83 @@ import math
 import stdio, stdarray, stdrandom, stddraw, stdaudio  # type: ignore
 import gamewindow as gw
 
-# X_MIN, X_MAX, Y_MIN, Y_MAX, FPS -> gamewindow.py
+SHOOTER_SIZE = 0.5
+SHOOTER_SPEED = 0.2
 
 class Shooter:
-    x : float
-    y : float
-    
-    def initial(shooter, pos_x, pos_y):
-        shooter.x = pos_x
-        shooter.y = pos_y
+    def __init__(self, pos_x, pos_y):
+        self.x = pos_x
+        self.y = pos_y
+        self.size = SHOOTER_SIZE
+        self.speed = SHOOTER_SPEED
+        self.color = stddraw.RED
+        self.health = 100
 
-        # initial position
-        pos_x = gw.X_MAX/2
-        pos_y = 0.1
-
-        shooter.size = 0.5
-        shooter.speed = 0.2
-        shooter.color = stddraw.RED
-        shooter.health = 100
-        
-    def draw(shooter):
-        
-        image_path = Picture('shooter.JPG')
-        stddraw.picture(image_path,pos_x,pos_y) 
+    def draw(self):
+        # Draw the shooter image
+        image_path = 'shooter.JPG'  
+        stddraw.picture(image_path, self.x, self.y)
 
         # Draw Health bar
-
         bar_x = 0.1
         bar_y = 0.02
-        width = shooter.x - bar_x/2
-        height = shooter.y + shooter.size +0.05
+        width = self.x - bar_x / 2
+        height = self.y + self.size + 0.05
 
         # Health Bar background
-        stddraw.setPenColor.(stddarray.BLUE)
-        stddraw.filledRectangle(width,height,bar_x,bar_y)
+        stddraw.setPenColor(stddraw.BLUE)
+        stddraw.filledRectangle(width, height, bar_x, bar_y)
 
         # Health color
-        health_width = bar_x * (shooter.health/50)
+        health_width = bar_x * (self.health / 100)  
         health_color = stddraw.GREEN
 
-        if shooter.health < 50:
+        if self.health < 50:
             health_color = stddraw.YELLOW
-        elif shooter.health < 20:
+        if self.health < 20:
             health_color = stddraw.RED
-            
-        stddraw.setPenColor(Health_Color)
-        stddraw.filledRectangle(width,height,health_width,bar_y)
+
+        stddraw.setPenColor(health_color)
+        stddraw.filledRectangle(width, height, health_width, bar_y)
 
         # Health bar border
         stddraw.setPenColor(stddraw.BLACK)
-        stddraw.rectangle(width,height,bar_x,bar_y)
-    
-    def damage(shooter, damage):
-        shooter.health =max(0, shooter.health-damage)
-        if shooter.health == 0:
+        stddraw.rectangle(width, height, bar_x, bar_y)
+
+    def damage(self, damage):
+        self.health = max(0, self.health - damage)
+        if self.health == 0:
             stdio.writeln("Shooter is dead!")
 
-            
 def main():
-
     stddraw.clear()
     
+    # Initialize shooter at the center bottom of the window
+    shooter = Shooter(gw.X_MAX / 2, 0.1)
+
     # main loop
     while True:
         if stddraw.hasNextKeyTyped():
             key = stddraw.nextKeyTyped()
-            if key == stddraw.KEYUP:
-                pos_y += SHOOTER_SPEED
-            elif key == stddraw.KEYDOWN:
-                pos_y -= SHOOTER_SPEED
-            elif key == stddraw.K_LEFT:
-                pos_x -= SHOOTER_SPEED
-            elif key == stddraw.K_RIGHT:
-                pos_x += SHOOTER_SPEED
+            if key == stddraw.KEY_UP:
+                shooter.y += shooter.speed
+            elif key == stddraw.KEY_DOWN:
+                shooter.y -= shooter.speed
+            elif key == stddraw.KEY_LEFT:
+                shooter.x -= shooter.speed
+            elif key == stddraw.KEY_RIGHT:
+                shooter.x += shooter.speed
             elif key == 'd':
                 shooter.damage(10)
             elif key == stddraw.K_ESCAPE:
                 break
                     
         # Boundary conditions
-        pos_x = max(SHOOTER_SIZE/2,min(gw.X_MAX - SHOOTER_SIZE/2, pos_x))
-        pos_y = max(SHOOTER_SIZE/2,min(gw.Y_MAX - SHOOTER_SIZE/2, pos_y))
+        shooter.x = max(shooter.size / 2, min(gw.X_MAX - shooter.size / 2, shooter.x))
+        shooter.y = max(shooter.size / 2, min(gw.Y_MAX - shooter.size / 2, shooter.y))
 
         shooter.draw()
-            
         stddraw.show(20)
-
 
 if __name__ == "__main__":
     main()
