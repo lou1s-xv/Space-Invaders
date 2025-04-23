a009a3f52ec488d4375a2f4a4e0a6000debb0dff
lou1s-xv <107552608+lou1s-xv@users.noreply.github.com>
2025-04-23 16:14:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 import sys
 import math
 import stdio, stdarray, stdrandom, stddraw, stdaudio  # type: ignore
-
+import enemies as en
+import missiles as ms
+from shooter import Shooter
+import gamewindow as gw
 
 def main() -> None:  # Need the return type for mypy to type-check the body
 
-    stdio.writeln("hello world")
+    #title screen
+    shooter = Shooter(...)
+    enemies = []
+    en_missiles = []
+    missiles = []
+    lvl_num = 1
+    while True:
+        enemies = en.load_level(lvl_num)
+        while len(enemies) != 0:
+            v_x = animate_form(enemies)
+            shooter.update_pos()
+            for i in len(missiles):
+                missiles[i].update()
+            for i in len(en_missiles):
+                en_missiles[i].update()
+            ms.detect_collisions(missiles, enemies)
+            ms.player_damage(en_missiles, shooter)
+            gw.show_window()
+        lvl_num += 1
 
 
 if __name__ == "__main__":
