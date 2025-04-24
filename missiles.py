7874230d481d8d3aac82df1daa5eacea88a5168d
lou1s-xv <107552608+lou1s-xv@users.noreply.github.com>
2025-04-24 20:01:47 +0200

    Update missiles.py

--- missiles.py
+++ missiles.py
@@ -4,8 +4,9 @@ import stdio, stdrandom, stddraw, stdaudio, stdarray
 import env
 import enemies as en
 import gamewindow as gw
-import constants as con
+import constants as cons
 import threading
+import shooter as sh
 
 # X_MIN, X_MAX, Y_MIN, Y_MAX, FPS -> gamewindow.py
 
@@ -14,6 +15,7 @@ MISSILE_SIZE = 0.1
 MISSILE_COLOR = stddraw.RED
 MISSILE_VELOCITY = 3 # this can be experimented with (in units/s)
 PI = math.pi
+ENEMY_POINT_VALUE = 10
 
 class Missile:
 
@@ -35,37 +37,84 @@ class Missile:
         stddraw.filledCircle(self.x, self.y, MISSILE_SIZE)
 
 
-def detect_collision(missiles, enemies): # destroys missiles and enemies in collisions, as well as out of bounds missiles
-    for i in range(len(missiles) -1, -1, -1):
+#def detect_collision(missiles, enemies, shooter): # destroys missiles and enemies in collisions, as well as out of bounds missiles
+#    for i in range(len(missiles)):
+#        if missiles[i].x < gw.X_MIN or missiles[i].x > gw.X_MAX or missiles[i].y < gw.Y_MIN or missiles[i].y > gw.Y_MAX:
+#            missiles.pop(i) 
+#        else:
+#            for k in range(env.BUNKER_NR):
+#                if gw.X_MIN + (env.BUNKER_WIDTH * (2 * k + 1)) < missiles[i].x < gw.X_MIN + (env.BUNKER_WIDTH * (2 * (k + 1))) and gw.Y_MIN + env.BUNKER_DIST < missiles[i].y < gw.Y_MIN + env.BUNKER_DIST + env.BUNKER_THICKNESS:
+#                    missiles.pop(i)
+#                    break
+#            for k in range(len(enemies)):
+#                if math.sqrt((missiles[i].x - enemies[k].x) ** 2 + (missiles[i].y - enemies[k].y) ** 2) < cons.ENEMY_SIZE / 2:
+#                    if enemies[k].pic == "mystery.png":
+#                        if shooter.health < 3:
+#                            shooter.health += 1
+#                    missiles.pop(i)
+#                    enemies.pop(k)
+#                    gw.add_points(ENEMY_POINT_VALUE)
+#                    break
+
+def detect_collision(missiles, enemies, shooter):
+    i = 0
+    while i < (len(missiles)) and i >= 0:
         if missiles[i].x < gw.X_MIN or missiles[i].x > gw.X_MAX or missiles[i].y < gw.Y_MIN or missiles[i].y > gw.Y_MAX:
-            missiles.pop(i) 
+            missiles.pop(i)
+            i -= 1
         else:
             for k in range(env.BUNKER_NR):
+                flag = False
                 if gw.X_MIN + (env.BUNKER_WIDTH * (2 * k + 1)) < missiles[i].x < gw.X_MIN + (env.BUNKER_WIDTH * (2 * (k + 1))) and gw.Y_MIN + env.BUNKER_DIST < missiles[i].y < gw.Y_MIN + env.BUNKER_DIST + env.BUNKER_THICKNESS:
                     missiles.pop(i)
+                    i -= 1
+                    flag = True
                     break
-            for k in range(len(enemies)):
-                if math.sqrt((missiles[i].x - enemies[k].x) ** 2 + (missiles[i].y - enemies[k].y) ** 2) < con.ENEMY_SIZE / 2:
-                    enemies.pop(k)
+            if flag:
+                continue
+            k = 0
+            while k < len(enemies) and k >= 0:
+                if math.sqrt((missiles[i].x - enemies[k].x) ** 2 + (missiles[i].y - enemies[k].y) ** 2) < cons.ENEMY_SIZE / 2:
+                    if enemies[k].pic == "mystery.png":
+                        if shooter.health < 3:
+                            shooter.health += 1
                     missiles.pop(i)
-                    gw.add_points(10)
-                    #threading.Thread(target=stdaudio.playFile, args=("explosion",)).start()
+                    if isinstance(enemies[k], en.Boss):
+                        enemies[k].health -= 1
+                        if enemies[k].health == 0:
+                            enemies.pop(k)
+                    else:
+                        enemies.pop(k)
+                    k -= 1
+                    i -= 1
+                    gw.add_points(ENEMY_POINT_VALUE)
                     break
+                k += 1
+            
+        i += 1
 
 def player_damage(missiles, shooter):
-    for i in range(len(missiles)):
+    i = 0
+    while i < (len(missiles)) and i >= 0:
         if missiles[i].x < gw.X_MIN or missiles[i].x > gw.X_MAX or missiles[i].y < gw.Y_MIN or missiles[i].y > gw.Y_MAX:
-            missiles.pop(i) 
+            missiles.pop(i)
+            i -= 1
         else:
             for k in range(env.BUNKER_NR):
+                flag = False
                 if gw.X_MIN + (env.BUNKER_WIDTH * (2 * k + 1)) < missiles[i].x < gw.X_MIN + (env.BUNKER_WIDTH * (2 * (k + 1))) and gw.Y_MIN + env.BUNKER_DIST < missiles[i].y < gw.Y_MIN + env.BUNKER_DIST + env.BUNKER_THICKNESS:
                     missiles.pop(i)
+                    i -= 1
+                    flag = True
                     break
-            if math.sqrt((missiles[i].x - shooter.x) ** 2 + (missiles[i].y - con.SHOOTER_DIST) ** 2) <= con.SHOOTER_SIZE:
+            if flag:
+                continue
+            if math.sqrt((missiles[i].x - shooter.x) ** 2 + (missiles[i].y - cons.SHOOTER_DIST) ** 2) <= cons.SHOOTER_SIZE:
                 missiles.pop(i)
-                shooter.health -= 1
-                stddraw.playFile("explosion.wav")
+                shooter.damage()
+                i -= 1
                 break
+        i += 1
 # TODO: make module tests
 
 def main():
